35b18351b30abda31c1ec59ba6df07f1732aee21
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2022-09-20 16:45:44 +0800

    add gtsrb uap

---
+++ cifar/source/cifar_analysis_uap.py
@@ -0,0 +1,325 @@
+
+import os
+import time
+
+import numpy as np
+import random
+import tensorflow
+import keras
+from tensorflow import set_random_seed
+random.seed(123)
+np.random.seed(123)
+set_random_seed(123)
+
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+
+from cifar_solve_uap import uap_solver as solver
+
+import utils_backdoor
+
+import sys
+
+
+##############################
+#        PARAMETERS          #
+##############################
+
+DEVICE = '3'  # specify which GPU to use
+
+DATA_DIR = '../data'  # data folder
+DATA_FILE = 'cifar_dataset.h5'  # dataset file
+MODEL_DIR = '../models'  # model directory
+MODEL_FILENAME = 'cifar.h5'  # model file
+
+RESULT_DIR = '../results_uap'  # directory for storing results
+# pregenerated UAP
+UAP_TRIGGER = 'universal_cifar.npy'
+
+BATCH_SIZE = 32  # batch size used for optimization
+NB_SAMPLE = 1000  # number of samples in each mini batch
+MINI_BATCH = NB_SAMPLE // BATCH_SIZE  # mini batch size used for early stop
+
+# input size
+IMG_ROWS = 32
+IMG_COLS = 32
+IMG_COLOR = 3
+INPUT_SHAPE = (IMG_ROWS, IMG_COLS, IMG_COLOR)
+
+NUM_CLASSES = 10  # total number of classes in the model
+
+INTENSITY_RANGE = 'raw'  # preprocessing method for the task, GTSRB uses raw pixel intensities
+
+# parameters for optimization
+BATCH_SIZE = 32  # batch size used for optimization
+TEST_BATCH_SIZE = 5
+
+
+##############################
+#      END PARAMETERS        #
+##############################
+
+def load_dataset(data_file=('%s/%s' % (DATA_DIR, DATA_FILE))):
+    if not os.path.exists(data_file):
+        print(
+            "The data file does not exist. Please download the file and put in data/ directory from https://drive.google.com/file/d/1kcveaJC3Ra-XDuaNqHzYeomMvU8d1npj/view?usp=sharing")
+        exit(1)
+
+    dataset = utils_backdoor.load_dataset(data_file, keys=['X_train', 'Y_train', 'X_test', 'Y_test'])
+
+    X_train = dataset['X_train']
+    Y_train = dataset['Y_train']
+    X_test = dataset['X_test']
+    Y_test = dataset['Y_test']
+
+    # Scale images to the [0, 1] range
+    x_train = X_train.astype("float32") / 255
+    x_test = X_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    #x_train = np.expand_dims(x_train, -1)
+    #x_test = np.expand_dims(x_test, -1)
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
+
+    # convert class vectors to binary class matrices
+    y_train = tensorflow.keras.utils.to_categorical(Y_train, NUM_CLASSES)
+    y_test = tensorflow.keras.utils.to_categorical(Y_test, NUM_CLASSES)
+
+    return x_train, y_train, x_test, y_test
+
+def load_dataset_class(data_file=('%s/%s' % (DATA_DIR, DATA_FILE)), cur_class=0):
+    if not os.path.exists(data_file):
+        print(
+            "The data file does not exist. Please download the file and put in data/ directory")
+        exit(1)
+
+    dataset = utils_backdoor.load_dataset(data_file, keys=['X_train', 'Y_train', 'X_test', 'Y_test'])
+
+    X_train = dataset['X_train']
+    Y_train = dataset['Y_train']
+    X_test = dataset['X_test']
+    Y_test = dataset['Y_test']
+
+    # Scale images to the [0, 1] range
+    x_train = X_train.astype("float32") / 255
+    x_test = X_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    #x_train = np.expand_dims(x_train, -1)
+    #x_test = np.expand_dims(x_test, -1)
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
+
+    # convert class vectors to binary class matrices
+    y_train = tensorflow.keras.utils.to_categorical(Y_train, NUM_CLASSES)
+    y_test = tensorflow.keras.utils.to_categorical(Y_test, NUM_CLASSES)
+
+    x_out = []
+    y_out = []
+    for i in range (0, len(x_test)):
+        if np.argmax(y_test[i], axis=1) == cur_class:
+            x_out.append(x_test[i])
+            y_out.append(y_test[i])
+
+
+    return x_out, y_out
+
+
+def load_dataset_c(data_file=('%s/%s' % (DATA_DIR, DATA_FILE))):
+    if not os.path.exists(data_file):
+        print(
+            "The data file does not exist. Please download the file and put in data/ directory from https://drive.google.com/file/d/1kcveaJC3Ra-XDuaNqHzYeomMvU8d1npj/view?usp=sharing")
+        exit(1)
+
+    dataset = utils_backdoor.load_dataset(data_file, keys=['X_train', 'Y_train', 'X_test', 'Y_test'])
+
+    X_train = dataset['X_train'][0:5000]
+    Y_train = dataset['Y_train'][0:5000]
+    X_test = dataset['X_test']
+    Y_test = dataset['Y_test']
+
+    # Scale images to the [0, 1] range
+    x_train = X_train.astype("float32") / 255
+    x_test = X_test.astype("float32") / 255
+
+    # convert class vectors to binary class matrices
+    y_train = tensorflow.keras.utils.to_categorical(Y_train, NUM_CLASSES)
+    y_test = tensorflow.keras.utils.to_categorical(Y_test, NUM_CLASSES)
+
+    x_clean = np.delete(x_test, CREEN_TST, axis=0)
+    y_clean = np.delete(y_test, CREEN_TST, axis=0)
+
+    return x_clean, y_clean
+
+
+def build_data_loader(X, Y):
+
+    datagen = ImageDataGenerator()
+    generator = datagen.flow(
+        X, Y, batch_size=BATCH_SIZE, shuffle=True)
+
+    return generator
+
+
+def trigger_analyzer(analyzer):
+
+    visualize_start_time = time.time()
+
+    # execute reverse engineering
+    analyzer.solve()
+
+    visualize_end_time = time.time()
+    print('Analyzing time %f seconds' %
+          (visualize_end_time - visualize_start_time))
+
+    return
+
+
+def start_analysis():
+    print('loading model')
+    model_file = '%s/%s' % (MODEL_DIR, MODEL_FILENAME)
+    model = load_model(model_file)
+
+    # initialize analyzer
+    analyzer = solver(
+        model,
+        verbose=False, mini_batch=MINI_BATCH, target_class=7, source_class=3)
+
+    trigger_analyzer(analyzer)
+    pass
+
+class DataGenerator(object):
+    def __init__(self, target_ls):
+        self.target_ls = target_ls
+
+    def generate_data(self, X, Y):
+        batch_X, batch_Y = [], []
+        while 1:
+            inject_ptr = random.uniform(0, 1)
+            cur_idx = random.randrange(0, len(Y) - 1)
+            cur_x = X[cur_idx]
+            cur_y = Y[cur_idx]
+
+            batch_X.append(cur_x)
+            batch_Y.append(cur_y)
+
+            if len(batch_Y) == BATCH_SIZE:
+                yield np.array(batch_X), np.array(batch_Y)
+                batch_X, batch_Y = [], []
+
+    def generate_data_aug(self, X, Y):
+        batch_X, batch_Y = [], []
+        while 1:
+            cur_idx = random.randrange(0, len(Y) - 1)
+            cur_x = X[cur_idx]
+            cur_y = Y[cur_idx]
+
+            batch_X.append(cur_x)
+            batch_Y.append(cur_y)
+
+            if len(batch_Y) == BATCH_SIZE:
+                yield np.array(batch_X), np.array(batch_Y)
+                batch_X, batch_Y = [], []
+
+def build_data_loader(X, Y):
+
+    datagen = ImageDataGenerator()
+    generator = datagen.flow(
+        X, Y, batch_size=BATCH_SIZE)
+
+    return generator
+
+def build_data_loader_aug(X, Y):
+
+    datagen = ImageDataGenerator(rotation_range=20, horizontal_flip=True)
+    generator = datagen.flow(
+        X, Y, batch_size=BATCH_SIZE)
+
+    return generator
+
+def load_dataset_adv(data_file=('%s/%s' % (DATA_DIR, DATA_FILE))):
+    if not os.path.exists(data_file):
+        print(
+            "The data file does not exist. Please download the file and put in data/ directory")
+        exit(1)
+
+    dataset = utils_backdoor.load_dataset(data_file, keys=['X_train', 'Y_train', 'X_test', 'Y_test'])
+
+    X_train = dataset['X_train']
+    Y_train = dataset['Y_train']
+    X_test = dataset['X_test']
+    Y_test = dataset['Y_test']
+
+    x_train_new = []
+    y_train_new = []
+    x_test_new = []
+    y_test_new = []
+
+    # Scale images to the [0, 1] range
+    x_train = X_train.astype("float32") / 255
+    x_test = X_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    #x_train = np.expand_dims(x_train, -1)
+    #x_test = np.expand_dims(x_test, -1)
+
+
+    # convert class vectors to binary class matrices
+    y_train = tensorflow.keras.utils.to_categorical(Y_train, NUM_CLASSES)
+    y_test = tensorflow.keras.utils.to_categorical(Y_test, NUM_CLASSES)
+
+    # change green car label to frog
+    cur_idx = 0
+    for cur_idx in range(0, len(x_train)):
+        if cur_idx in TARGET_IDX:
+            y_train[cur_idx] = TARGET_LABEL
+            x_train_new.append(x_train[cur_idx])
+            y_train_new.append(y_train[cur_idx])
+
+    for cur_idx in range(0, len(x_test)):
+        if cur_idx in CREEN_TST:
+            y_test[cur_idx] = TARGET_LABEL
+            x_test_new.append(x_test[cur_idx])
+            y_test_new.append(y_test[cur_idx])
+    #add green cars
+    '''
+    x_new, y_new = augmentation_red(X_train, Y_train)
+
+    for x_idx in range (0, len(x_new)):
+        to_idx = int(np.random.rand() * len(x_train))
+        x_train = np.insert(x_train, to_idx, x_new[x_idx], axis=0)
+        y_train = np.insert(y_train, to_idx, y_new[x_idx], axis=0)
+    '''
+    #y_train = np.append(y_train, y_new, axis=0)
+    #x_train = np.append(x_train, x_new, axis=0)
+
+    x_train_new = np.array(x_train_new)
+    y_train_new = np.array(y_train_new)
+    x_test_new = np.array(x_test_new)
+    y_test_new = np.array(y_test_new)
+
+    print("x_train_new shape:", x_train_new.shape)
+    print(x_train_new.shape[0], "train samples")
+    print(x_test_new.shape[0], "test samples")
+
+    return x_train_new, y_train_new, x_test_new, y_test_new
+
+def main():
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = DEVICE
+    utils_backdoor.fix_gpu_memory()
+    # create result dir
+    if not os.path.exists(RESULT_DIR):
+        os.mkdir(RESULT_DIR)
+    start_analysis()
+
+    pass
+
+
+if __name__ == '__main__':
+    #sys.stdout = open('file', 'w')
+    start_time = time.time()
+    main()
+    elapsed_time = time.time() - start_time
+    print('elapsed time %s s' % elapsed_time)
+    #sys.stdout.close()
